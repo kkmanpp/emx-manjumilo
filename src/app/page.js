7ecba221700cc190ba9567106f725297124495e7
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-17 12:44:33 +0800

    feat: crated product card & outline button

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,132 @@
+import Card from "@/components/Card";
 import Image from "next/image";
+import perfume from "../../public/perfume.jpg";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const PRODUCT_LIST = [
+		{
+			id: "56789",
+			title: "萬壽之露",
+			image: perfume,
+			primaryDesc:
+				"萬壽之露以本社自己培育的沖繩長壽食物青木瓜為主，加上玄米、米糠、海帶、海蘊，利用有益微生物群和本社獨有的發酵技術，最後提煉出含有多種多樣有益成分的健康提取液。",
+			subTitle: "推薦給下列人士:",
+			secondaryDesc: [
+				"鐘情安全、營養飲品的人士",
+				"需要美膚的人士",
+				"運動員，運動量大的人士",
+			],
+		},
+	];
+	return (
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+				<Image
+					className="dark:invert"
+					src="/next.svg"
+					alt="Next.js logo"
+					width={180}
+					height={38}
+					priority
+				/>
+				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+					<li className="mb-2">
+						Get started by editing{" "}
+						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+							src/app/page.js
+						</code>
+						.
+					</li>
+					<li>Save and see your changes instantly.</li>
+				</ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				<div className="flex gap-4 items-center flex-col sm:flex-row">
+					<a
+						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							className="dark:invert"
+							src="/vercel.svg"
+							alt="Vercel logomark"
+							width={20}
+							height={20}
+						/>
+						Deploy now
+					</a>
+					<a
+						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Read our docs
+					</a>
+				</div>
+				<div className="grid grid-cols-3 grid-flow-row gap-8 mobile:grid-cols-1">
+					{PRODUCT_LIST.map((product, index) => {
+						return (
+							<Card
+								key={product.id}
+								title={product.title}
+								image={product.image}
+								primaryDesc={product.primaryDesc}
+								subTitle={product.subTitle}
+								secondaryDesc={product.secondaryDesc}
+							></Card>
+						);
+					})}
+				</div>
+			</main>
+			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/file.svg"
+						alt="File icon"
+						width={16}
+						height={16}
+					/>
+					Learn
+				</a>
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/window.svg"
+						alt="Window icon"
+						width={16}
+						height={16}
+					/>
+					Examples
+				</a>
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Image
+						aria-hidden
+						src="/globe.svg"
+						alt="Globe icon"
+						width={16}
+						height={16}
+					/>
+					Go to nextjs.org →
+				</a>
+			</footer>
+		</div>
+	);
 }
---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import OutlineButton from "./OutlineButton";
+import Image from "next/image";
+
+export default function Card({
+	title,
+	image,
+	primaryDesc,
+	subTitle,
+	secondaryDesc,
+}) {
+	return (
+		<div className="grid grid-cols-1 grid-rows-[2rem_1fr_1fr_auto] items-center justify-center rounded-xl shadow-lg m-2">
+			<div className="bg-Green-50 text-Green-700 text-center h-full font-medium flex items-center justify-center">
+				<div>{title}</div>
+			</div>
+			<div>
+				<Image src={image} height={300} alt={title} className="w-full" />
+			</div>
+			<div className="text-Grey-800 text-start p-4">
+				<div>{primaryDesc}</div>
+				<div className="pt-4">
+					<div>{subTitle}</div>
+					{secondaryDesc.length &&
+						secondaryDesc.map((text) => {
+							return <div className="px-4">{text}</div>;
+						})}
+				</div>
+			</div>
+			<div className="grid grid-cols-[2fr_1fr]">
+				<div className="col-start-2 flex flex-col justify-end">
+					<OutlineButton
+						label="More"
+						onClick={console.log("press button")}
+					></OutlineButton>
+				</div>
+			</div>
+		</div>
+	);
+}
