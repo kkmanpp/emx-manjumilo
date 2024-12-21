fac6a64fe0d2934996117022fff9310b5e06c09f
jessie wong <jessie.wong@alphasolution.com.mo>
2024-12-21 15:36:28 +0800

    fix: language param error on home page

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { getDictionary } from "./dictionaries";
 import LinkButton from "@/components/LinkButton";
 
-export default async function Home({ params: { lang } }) {
+export default async function Home({ params }) {
+  const { lang } = await params;
   const t = await getDictionary(lang);
   return <div>Home</div>;
 }
