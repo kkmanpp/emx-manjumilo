711331f2838bb1755c481e2022ec807b73377158
eddieWork01 <eddiework01@icloud.com>
2024-12-29 12:31:54 +0800

    add loading

--- src/components/ProductDetailViewer.js
+++ src/components/ProductDetailViewer.js
@@ -1,30 +1,34 @@
 "use client";
 import ProductDetailCard from "@/components/ProductDetailCard";
 import { useEffect, useState } from "react";
+import { useLoading } from "@/context/LoadingContext";
 
 export default function ProductDetailViewer({ sku, lang, t }) {
-	const [product, setProduct] = useState(null);
-	const [error, setError] = useState("");
+  const [product, setProduct] = useState(null);
+  const [error, setError] = useState("");
+  const { startLoading, stopLoading } = useLoading();
 
-	useEffect(() => {
-		async function fetchProduct() {
-			setError("");
-			setProduct(null);
-			await fetch(`/api/products/${sku}`)
-				.then((response) => response.json())
-				.then((data) => setProduct(data.product))
-				.catch((err) => setError("Failed to get data"));
-		}
-		fetchProduct();
-	}, []);
+  useEffect(() => {
+    async function fetchProduct() {
+      setError("");
+      setProduct(null);
+      startLoading();
+      await fetch(`/api/products/${sku}`)
+        .then((response) => response.json())
+        .then((data) => setProduct(data.product))
+        .catch((err) => setError("Failed to get data"));
+      stopLoading();
+    }
+    fetchProduct();
+  }, []);
 
-	if (!product) return <div>loading</div>;
-	if (!product.length) return <div>No Such Product</div>;
-	if (error) return <div>{error}</div>;
+  if (!product) return;
+  if (!product.length) return <div>No Such Product</div>;
+  if (error) return <div>{error}</div>;
 
-	return (
-		<div>
-			<ProductDetailCard dictionary={t} lang={lang} product={product[0]} />
-		</div>
-	);
+  return (
+    <div>
+      <ProductDetailCard dictionary={t} lang={lang} product={product[0]} />
+    </div>
+  );
 }
--- src/components/ProductViewer.js
+++ src/components/ProductViewer.js
@@ -2,44 +2,48 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import ProductOverviewCard from "./ProductOverviewCard";
+import { useLoading } from "@/context/LoadingContext";
 
 export default function ProductViewer({ lang, dictionary }) {
-	const [productList, setProductList] = useState(null);
-	const [error, setError] = useState("");
-	const router = useRouter();
+  const [productList, setProductList] = useState(null);
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const { startLoading, stopLoading } = useLoading();
 
-	const handleOnClick = (id) => {
-		console.log(id);
-		router.push(`/products/${id}`);
-	};
+  const handleOnClick = (id) => {
+    console.log(id);
+    router.push(`/products/${id}`);
+  };
 
-	useEffect(() => {
-		async function fetchAllProducts() {
-			setError("");
-			setProductList(null);
-			await fetch("/api/all-products")
-				.then((response) => response.json())
-				.then((data) => setProductList(data.products))
-				.catch((err) => setError("Failed to get data"));
-		}
-		fetchAllProducts();
-	}, []);
+  useEffect(() => {
+    async function fetchAllProducts() {
+      setError("");
+      setProductList(null);
+      startLoading();
+      await fetch("/api/all-products")
+        .then((response) => response.json())
+        .then((data) => setProductList(data.products))
+        .catch((err) => setError("Failed to get data"));
+      stopLoading();
+    }
+    fetchAllProducts();
+  }, []);
 
-	if (!productList) return <div>loading</div>;
-	if (error) return <div>{error}</div>;
+  if (!productList) return;
+  if (error) return <div>{error}</div>;
 
-	return (
-		<div className="w-[45rem] grid grid-cols-2 gap-4 grid-flow-row">
-			{productList.map((product) => {
-				return (
-					<ProductOverviewCard
-						key={product.sku}
-						product={product}
-						lang={lang}
-						onClick={() => handleOnClick(product.sku)}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="w-[45rem] grid grid-cols-2 gap-4 grid-flow-row">
+      {productList.map((product) => {
+        return (
+          <ProductOverviewCard
+            key={product.sku}
+            product={product}
+            lang={lang}
+            onClick={() => handleOnClick(product.sku)}
+          />
+        );
+      })}
+    </div>
+  );
 }
