1abb442e508718760ee62af12458b18a2d7bb2f9
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-30 11:34:28 +0800

    added hot sale product viewers on homepage

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import { getDictionary } from "./dictionaries";
 import LinkButton from "@/components/LinkButton";
+import TrendingProductViewer from "@/components/TrendingProductViewer";
 
 export default async function Home({ params }) {
-  const { lang } = await params;
-  const t = await getDictionary(lang);
+	const { lang } = await params;
+	const t = await getDictionary(lang);
 
-  return <div></div>;
+	return (
+		<div>
+			<TrendingProductViewer lang={lang} />
+		</div>
+	);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,36 @@
 import LinkButton from "./LinkButton";
 import Image from "next/image";
 
-export default function Card({ title, image, primaryDesc }) {
-  return (
-    <div className="grid grid-cols-1 grid-rows-[2rem_1fr_1fr_auto] items-center justify-center rounded-xl shadow-lg m-2">
-      <div className="bg-Green-50 text-Green-700 text-center h-full font-medium flex items-center justify-center">
-        <div>{title}</div>
-      </div>
-      <div>
-        <Image src={image} height={300} alt={title} className="w-full" />
-      </div>
-      <div className="text-Grey-800 text-start p-4">
-        <div>{primaryDesc}</div>
-        {/* <div className="pt-4">
+export default function Card({ title, image, primaryDesc, onClick }) {
+	return (
+		<div className="grid grid-cols-1 grid-rows-[2rem_25rem_1fr_4rem] items-center justify-center rounded-xl shadow-lg m-2 bg-white">
+			<div className="bg-Green-50 text-Green-700 text-center h-full font-medium flex items-center justify-center">
+				<div>{title}</div>
+			</div>
+			<div>
+				<Image
+					src={image}
+					width={100}
+					height={100}
+					alt={title}
+					className="w-full object-contain"
+				/>
+			</div>
+			<div className="text-Grey-800 text-start p-4">
+				<div>{primaryDesc}</div>
+				{/* <div className="pt-4">
 					<div>{subTitle}</div>
 					{secondaryDesc.length &&
 						secondaryDesc.map((text) => {
 							return <div className="px-4">{text}</div>;
 						})}
 				</div> */}
-      </div>
-      <div className="grid grid-cols-[2fr_1fr]">
-        <div className="col-start-2 flex flex-col justify-end">
-          <LinkButton
-            label="More"
-            onClick={console.log("press button")}
-          ></LinkButton>
-        </div>
-      </div>
-    </div>
-  );
+			</div>
+			<div className="grid grid-cols-[2fr_1fr]">
+				<div className="col-start-2 flex flex-col justify-end">
+					<LinkButton label="More" onClick={onClick}></LinkButton>
+				</div>
+			</div>
+		</div>
+	);
 }
