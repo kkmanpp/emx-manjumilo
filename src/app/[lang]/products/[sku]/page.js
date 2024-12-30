d218ddccefe37ec284129ec395361c46855730f0
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-30 17:55:51 +0800

    added dynamic meta data

--- src/app/[lang]/products/[sku]/page.js
+++ src/app/[lang]/products/[sku]/page.js
@@ -2,6 +2,18 @@ import { getDictionary } from "./../../dictionaries";
 
 import ProductDetailViewer from "@/components/ProductDetailViewer";
 
+export async function generateMetadata({ params }) {
+	const sku = (await params).sku;
+
+	const product = await fetch(`http://localhost:3001/api/products/${sku}`, {
+		method: "GET",
+	}).then((res) => res.json());
+
+	return {
+		title: product.product[0].name["cht"],
+	};
+}
+
 export default async function Page({ params }) {
 	const { sku, lang } = await params;
 	const t = await getDictionary(lang);
