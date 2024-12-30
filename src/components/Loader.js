581740773bc67022e6abe3bcd665838dad643ffc
eddieWork01 <eddiework01@icloud.com>
2024-12-30 11:41:50 +0800

    fixed: loader causing infinite re-render; add: utils/time.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,24 @@
 "use client";
 import { useLoading } from "../context/LoadingContext";
+import Image from "next/image";
 
 export default function Loader({ children }) {
   const { isLoading } = useLoading();
 
-  return isLoading ? (
-    <div className="flex p-32 items-center justify-center">
-      <img src="/loading.gif" alt="Loading..." />
-    </div>
-  ) : (
-    <>{children}</>
+  return (
+    <>
+      {isLoading && (
+        <div className="flex p-32 items-center justify-center">
+          <Image
+            src="/loading.gif"
+            alt="Loading..."
+            layout="responsive"
+            width={1} // Aspect ratio numerator
+            height={1}
+          />
+        </div>
+      )}
+      <div style={{ display: isLoading ? "none" : "block" }}>{children}</div>
+    </>
   );
 }
