808d1445285e5e8a7c1ccc11aa1672fad011cf58
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-31 10:34:38 +0800

    allow player to be full screen

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -6,7 +6,9 @@ export default function VideoPlayer({ videoId }) {
 			height={215}
 			src={`https://www.youtube.com/embed/${videoId}`}
 			className="rounded-xl"
-			title="event"
+			title="event-video"
+			allowFullScreen
+			loading="lazy"
 		></iframe>
 	);
 }
