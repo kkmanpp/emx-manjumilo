3654b5883e41b04f6e192f7643403d8f2e7fda6b
JessieWong <jessie.wong@alphasolution.com.mo>
2024-12-31 11:02:25 +0800

    image optimizing

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
 export default function Loader() {
-  return (
-    <div className="flex p-32 items-center justify-center">
-      <Image
-        src="/loading.gif"
-        alt="Loading..."
-        layout="responsive"
-        width={1}
-        height={1}
-      />
-    </div>
-  );
+	return (
+		<div className="flex p-32 items-center justify-center">
+			<Image
+				src="/loading.gif"
+				alt="Loading..."
+				width={1}
+				height={1}
+				className="object-contain"
+			/>
+		</div>
+	);
 }
