e034e5023cbfa250da731634716bb8e86fc23dc9
eddieWork01 <eddiework01@icloud.com>
2025-01-01 16:55:07 +0800

    fix: refactor all products page and product detail page to apply SSR

--- src/app/[lang]/products/page.js
+++ src/app/[lang]/products/page.js
@@ -4,10 +4,17 @@ import { getDictionary } from "../dictionaries";
 export default async function Page({ params }) {
   const { lang } = await params;
   const t = await getDictionary(lang);
-  // console.log("t", t);
+  const products = await fetch(`${process.env.BASEURL}/api/all-products`).then(
+    (res) => res.json()
+  );
+
   return (
     <div>
-      <ProductViewer dictionary={t} lang={lang} />
+      <ProductViewer
+        dictionary={t}
+        lang={lang}
+        productList={products.products}
+      />
     </div>
   );
 }
