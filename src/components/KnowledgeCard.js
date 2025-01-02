ba5e33a7f44adbad42789dfb47983225c7c7d600
JessieWong <jessie.wong@alphasolution.com.mo>
2025-01-02 18:47:35 +0800

    fixed mobile ui

--- src/components/KnowledgeCard.js
+++ src/components/KnowledgeCard.js
@@ -1,10 +1,10 @@
 export default function KnowledgeCard({ title, children }) {
-  return (
-    <div className="h-auto w-full rounded-[.6rem] grid grid-rows-[1fr_auto] shadow-xl bg-white dark:text-Grey-900">
-      <div className="border-l-4 border-Green-400 font-semibold px-2 m-4">
-        {title}
-      </div>
-      <div className="mb-2 mx-2">{children}</div>
-    </div>
-  );
+	return (
+		<div className="h-auto w-full rounded-[.6rem] grid grid-rows-[1fr_auto] shadow-xl bg-white dark:text-Grey-900">
+			<div className="border-l-4 border-Green-400 font-semibold px-2 m-4">
+				{title}
+			</div>
+			<div className="mb-2 mx-2">{children}</div>
+		</div>
+	);
 }
