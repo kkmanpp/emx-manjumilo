0095b322a005a4734c383d1e35a5f564aa980a22
jessie wong <jessie.wong@alphasolution.com.mo>
2025-01-04 19:08:08 +0800

    modified product page ui

--- src/app/[lang]/products/components/ProductViewer.js
+++ src/app/[lang]/products/components/ProductViewer.js
@@ -3,22 +3,22 @@ import ProductOverviewCard from "./ProductOverviewCard";
 import { useRouter } from "next/navigation";
 
 export default function ProductViewer({ productList, lang, dictionary }) {
-	const router = useRouter();
+  const router = useRouter();
 
-	const handleOnClick = (sku) => {
-		router.push(`/products/${sku}`);
-	};
-
-	return (
-		<div className="w-[45rem] grid grid-cols-2 gap-4 grid-flow-row mobile:w-full mobile:grid-cols-1 mobile:gap-2">
-			{productList.map((product) => (
-				<ProductOverviewCard
-					key={product.sku}
-					product={product}
-					lang={lang}
-					onClick={() => handleOnClick(product.sku)}
-				/>
-			))}
-		</div>
-	);
+  const handleOnClick = (sku) => {
+    router.push(`/products/${sku}`);
+  };
+  // largeMobile:my-4 largeMobile:w-[30rem] tablet:w-[35rem]
+  return (
+    <div className="largeMobile:w-auto tablet:px-4 laptop:px-8 grid largeMobile:grid-cols-2 largeMobile:gap-4 grid-flow-row w-full grid-cols-1 gap-2">
+      {productList.map((product) => (
+        <ProductOverviewCard
+          key={product.sku}
+          product={product}
+          lang={lang}
+          onClick={() => handleOnClick(product.sku)}
+        />
+      ))}
+    </div>
+  );
 }
