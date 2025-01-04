e4ef6123010b35285d95369d21e366d3d640b2dc
jessie wong <jessie.wong@alphasolution.com.mo>
2025-01-04 18:37:34 +0800

    modified home page ui

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,31 +2,31 @@
 import { useEffect, useState } from "react";
 
 export default function VideoPlayer({ videoId }) {
-	//5XDNHx2MQ40
-	// const [isMobile, setIsMobile] = useState(false);
-	// let isMobile =
-	// 	Math.min(window.screen.width, window.screen.height) < 768 ||
-	// 	navigator.userAgent.indexOf("Mobi") > -1;
+  //5XDNHx2MQ40
+  // const [isMobile, setIsMobile] = useState(false);
+  // let isMobile =
+  // 	Math.min(window.screen.width, window.screen.height) < 768 ||
+  // 	navigator.userAgent.indexOf("Mobi") > -1;
 
-	// useEffect(() => {
-	// 	if (typeof window !== "undefined") {
-	// 		let result =
-	// 			Math.min(window.screen.width, window.screen.height) < 768 ||
-	// 			navigator.userAgent.indexOf("Mobi") > -1;
-	// 		console.log(result);
-	// 		setIsMobile(result);
-	// 	}
-	// }, []);
+  // useEffect(() => {
+  // 	if (typeof window !== "undefined") {
+  // 		let result =
+  // 			Math.min(window.screen.width, window.screen.height) < 768 ||
+  // 			navigator.userAgent.indexOf("Mobi") > -1;
+  // 		console.log(result);
+  // 		setIsMobile(result);
+  // 	}
+  // }, []);
 
-	return (
-		<iframe
-			width={420}
-			height={325}
-			src={`https://www.youtube.com/embed/${videoId}`}
-			className="rounded-xl mobile:w-[22rem]"
-			title="event-video"
-			allowFullScreen
-			loading="lazy"
-		></iframe>
-	);
+  return (
+    <iframe
+      width={420}
+      height={325}
+      src={`https://www.youtube.com/embed/${videoId}`}
+      className="rounded-xl w-[22rem] largeMobile:w-[29rem] tablet:w-[25rem] laptop:w-[28rem]"
+      title="event-video"
+      allowFullScreen
+      loading="lazy"
+    ></iframe>
+  );
 }
